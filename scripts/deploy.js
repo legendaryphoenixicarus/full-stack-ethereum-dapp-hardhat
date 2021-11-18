45288dae1ab5c73651c047463c47a0ba0196f208
legendaryphoenixicarus <legendary.phoenix.icarus@gmail.com>
2021-11-18 23:10:44 +0900

    update: auto save contract addresses in the config file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require('fs');
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
@@ -18,12 +19,26 @@ async function main() {
 
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
+
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");  
   
   await greeter.deployed();
   await token.deployed();
+  await ndToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
+  console.log("NDToken deployed to:", ndToken.address);
+
+  let config = `
+  export const greeterAddress = "${greeter.address}"
+  export const tokenAddress = "${token.address}"
+  export const ndTokenAddress = "${ndToken.address}"
+  `;
+
+  let data = JSON.stringify(config);
+  fs.writeFileSync('./src/config.js', JSON.parse(data));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+  export const greeterAddress = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+  export const tokenAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
+  export const ndTokenAddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
+  
